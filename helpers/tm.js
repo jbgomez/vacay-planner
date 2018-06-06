a8f2afbde3d3f61a584f24eeb4b0c11d36c8f6a5
joseph <jbgomez88@gmail.com>
2018-06-06 09:36:02 -0700

    Refactor to implicity inherit options

--- helpers/tm.js
+++ helpers/tm.js
@@ -4,21 +4,16 @@ const config = require('../config.js');
 const key = config.TM_KEY
 
 const tm = (options, cb) => {
+  options.apikey = key;
+
   request.get({
     method: 'GET',
     uri: 'https://app.ticketmaster.com/discovery/v2/events.json',
-    qs: {
-      startDateTime: options.startDate,
-      endDateTime: options.endDate,
-      city: options.city,
-      stateCode: options.stateCode,
-      size: options.size,
-      apikey: key
-    },
+    qs: options,
     json: true
   })
-  .then((data) => cb(data._embedded.events))
-  .catch((xhr, status, err) => console.log(err))
+  .then((data) => cb(data._embedded ? data._embedded.events : null))
+  .catch(err => cb(err));
 };
 
 module.exports = tm;
