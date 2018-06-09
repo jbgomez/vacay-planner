6d4cfa421d217113388770bae426714e1a6d2a91
Hubert Nimitanakit <hcnimi@gmail.com>
2018-06-09 14:20:20 -0700

    Render MyTrips summary list component on LandingPageBody.

---
+++ client/src/components/LandingPage/LandingPageMyTrips.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Card, Grid, Form, Button, Segment, Header } from 'semantic-ui-react';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import MomentLocaleUtils, {formatDate, parseDate} from 'react-day-picker/moment';
+import LocationSearchInput from './LocationSearchInput.jsx';
+
+class LandingPageMyTrips extends React.Component {
+
+  render () {
+    return (
+      <Grid verticalAlign='middle' style={ {marginTop: 50} }>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450}}>
+          <Card centered fluid>
+            <Card.Content header='Vacation Planner' />
+            <Card.Content extra>
+              <Form onSubmit={() => props.handleSubmit(props.history)}>
+                <Header as='h2' color='red' textAlign='center'>
+                  {' '}Where to?
+                </Header>
+
+                <Segment stacked>
+                  City, State:<br />
+                  <LocationSearchInput
+                    address={props.address}
+                    handleAddressChange={props.handleAddressChange}
+                    handleAddressSelect={props.handleAddressSelect}
+                  />
+                  <br /><br />
+                  Start Date:<br />
+                  <DayPickerInput
+                    onDayChange={props.handleStartDayChange}
+                    formatDate={formatDate}
+                    parseDate={parseDate}
+                    placeholder={`${formatDate(new Date())}`}
+                    value={`${formatDate(new Date())}`}
+                  />
+                  <br /><br />
+                  End Date:<br />
+
+                  <DayPickerInput
+                    onDayChange={props.handleEndDayChange}
+                    formatDate={formatDate}
+                    parseDate={parseDate}
+                    placeholder={`${formatDate(new Date(new Date().valueOf() + 60 * 60 * 24 * 1000))}`}
+                    value={`${formatDate(new Date(new Date().valueOf() + 60 * 60 * 24 * 1000))}`}
+                  />
+                  <br /><br />
+
+                  <Form.Button content='Submit' fluid size='large' />
+                </Segment>
+              </Form>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default LandingPageBody;
