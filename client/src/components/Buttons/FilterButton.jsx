b5338363b5acf71264fa5f701621fecc51ea9f6c
Yang Chen <y.chen.pf@gmail.com>
2018-06-11 21:24:08 -0700

    add ratings to filter

--- client/src/components/Buttons/FilterButton.jsx
+++ client/src/components/Buttons/FilterButton.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import { Button, Modal } from 'semantic-ui-react'
+import { Button, Modal, Rating } from 'semantic-ui-react'
 
 class FilterButton extends Component {
   constructor(props) {
     super(props);
     this.state = {size: 'small',
-                  open: false };
+                  open: false,
+                  rating: 0};
     this.show = this.show.bind(this);
     this.close = this.close.bind(this);
     this.onFilter = this.onFilter.bind(this);
+    this.recordRating = this.recordRating.bind(this);
   }
 
   show() {
@@ -23,14 +25,19 @@ class FilterButton extends Component {
     this.close();
   }
 
+  recordRating(event, data) {
+    this.setState({rating: data.rating});
+  }
+
   render() {
     return (
       <div style={{position: 'absolute', top: 0, right: '0.85em'}} >
         <Button onClick={this.show}>Filter</Button>
         <Modal size={this.state.size} open={this.state.open} onClose={this.close}>
-          <Modal.Header>Filter</Modal.Header>
+          <Modal.Header>Select Filter Criteria</Modal.Header>
           <Modal.Content>
-            <p>Select Filter Criteria</p>
+            <p>Rating</p>
+            <Rating maxRating={5} clearable onRate = {this.recordRating} />
           </Modal.Content>
           <Modal.Actions>
             <Button positive onClick = {this.onFilter}>Yes</Button>
