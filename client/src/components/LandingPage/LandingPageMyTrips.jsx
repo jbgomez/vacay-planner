208613119898bdbe096f1bae8c699e302551a7d7
Hubert Nimitanakit <hcnimi@gmail.com>
2018-06-12 08:47:11 -0700

    (bug) add 'return' statement to map

--- client/src/components/LandingPage/LandingPageMyTrips.jsx
+++ client/src/components/LandingPage/LandingPageMyTrips.jsx
@@ -5,20 +5,18 @@ import MyTripsEntry from './MyTripsEntry.jsx';
 
 const LandingPageMyTrips = (props) => {
   return (
-    <Grid verticalAlign='middle' style={ {marginTop: 50} }>
-      <Grid.Row>
-        <Grid.Column style={{ maxWidth: 450}}>
-          <Card centered fluid>
-            <Card.Content header='Upcoming Trips'/>
-            <Card.Content extra>
-              {props.allTrips.map(trip => {
-                <MyTripsEntry trip={trip}/>
-              })}
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <Grid.Row>
+      <Grid.Column style={{ maxWidth: 450}}>
+        <Card centered fluid>
+          <Card.Content header='Upcoming Trips'/>
+          <Card.Content extra>
+            {props.trips.map(trip => {
+              return(<MyTripsEntry trip={trip}/>);
+            })}
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+    </Grid.Row>
   );
 }
 
